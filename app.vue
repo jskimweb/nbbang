058ab770b2dcb83a27a51258268aba8bde567548
jskimweb <jskimweb@gmail.com>
2023-02-02 03:16:49 +0900

    반응형 타이틀 추가

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <h1>엔빵 - 더치페이 계산</h1>
+    <Head>
+      <Title>{{ title }}</Title>
+    </Head>
+
+    <h1>{{ title }}</h1>
   </div>
 </template>
+
+<script setup lang="ts">
+const title = ref('엔빵 - 더치페이 계산');
+</script>
