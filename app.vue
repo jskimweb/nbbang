732e712de1d7b2d17adcd00c54f827f9242845e0
jskimweb <jskimweb@gmail.com>
2023-02-07 02:52:11 +0900

    head & 환경변수 셋팅 및 카카오톡 공유 구현

--- app.vue
+++ app.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
-    <Head>
-      <Title>{{ title }}</Title>
-    </Head>
-    <h1>{{ title }}</h1>
     <TheForm />
+    <button @click="shareToKakaotalk">카카오톡 공유</button>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import TheForm from './components/TheForm.vue';
 
-const title = ref('엔빵 - 더치페이 계산');
+const config = useRuntimeConfig();
+
+initKakao();
+
+function initKakao() {
+  if (process.client) Kakao.init(config.kakaoJavascriptKey);
+}
+function shareToKakaotalk() {
+  const options = {
+    objectType: 'text',
+    text: '공유합니다.',
+    link: {
+      mobileWebUrl: 'http://localhost:8080',
+      webUrl: 'http://localhost:8080',
+    }
+  };
+  Kakao.Share.sendDefault(options);
+}
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,22 @@
 export default defineNuxtConfig({
-
+	app: {
+		head: {
+			title: '엔빵 - 더치페이 계산',
+			meta: [
+				{ name: 'description', content: '더치페이를 계산하고 공유해보세요!' }
+			],
+			script: [
+				{
+					src: 'https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js',
+					integrity:"sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx",
+					crossorigin:"anonymous"
+				}
+			]
+		}
+	},
+	runtimeConfig: {
+		public: {
+			kakaoJavascriptKey: ''
+		}
+	}
 })
